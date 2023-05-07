03dc493922fe62cf52bda572336b086c58cd963a
hyuk12 <hotman7025@naver.com>
2023-05-08 02:33:34 +0900

    feat: input 과 카드 그리드 레이아웃
    input 창 만들고 해당 컴포넌트들 기준으로 섹션을 나누어
    섹션별로 start부분에 자동으로 멈춤이 되는 기능이 들어감

---
+++ trip_location/src/components/contents/main/LocationCard.js
@@ -0,0 +1,27 @@
+import {Card, CardActionArea, CardContent, CardMedia, Typography} from '@mui/material';
+
+const LocationCard = ({ image, title, description }) => {
+    return (
+        <Card>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    alt={title}
+                    height="140"
+                    image={image}
+                    title={title}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {title}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+};
+
+export default LocationCard;

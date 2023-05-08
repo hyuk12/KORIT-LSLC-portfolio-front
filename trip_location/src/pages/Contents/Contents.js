a369e15108194d581bb2eb0e793dcba6021b85e5
dlgustn3768 <dlgustn3768@naver.com>
2023-05-08 12:39:56 +0900

    Fix: 오늘 날짜(local)를 기준으로 최대 3달까지 시작일을 잡을수 있고, 시작일의 기준으로 한달까지 일정을 정할 수 있도록 수정함.

--- trip_location/src/pages/Contents/Contents.js
+++ trip_location/src/pages/Contents/Contents.js
@@ -1,9 +1,9 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
-import React from 'react';
+import React, { useState } from 'react';
 import Calendar from '../../components/Calendar/Calendar';
 import Map from '../../components/contents/Map/Map';
-
+import dayjs from 'dayjs';
 
 const container = css`
   display: flex;
@@ -46,12 +46,40 @@ const rightsidebar = css`
 
 
 const Contents = ({ destinationTitle }) => {
+  const [startDay, setStartDay] = useState(dayjs());
+  const [endDay, setEndDay] = useState(dayjs().add(1, 'day'));
+  const [totalDate, setTotalDate] =useState(1);
+
+  const startDayHandle = (newValue) => {
+    setStartDay(newValue);
+    setTotalDate(endDay.diff(newValue, 'day') + 1);
+  }
+
+  const endDayHanlde = (newValue) => {
+    setEndDay(newValue);
+    setTotalDate(newValue.diff(startDay, 'Day') + 1)
+  }
+
+  const resetDay = () => {
+    setStartDay(dayjs());
+    setEndDay(dayjs().add(1,'day'));
+    setTotalDate(1);
+  }
+  
   return (
+    
 
     <div css={container}>
       <div css={leftsidebar}>
         <div>여행장소 이름</div>
-        <Calendar />
+        <button onClick={resetDay}>Reset Start Day</button>
+        <Calendar 
+          startDay={startDay}
+          endDay={endDay}
+          totalDate={totalDate}
+          onStartDayChange={startDayHandle}
+          onEndDayChange={endDayHanlde}
+        />
       </div>
       <div css={main}><Map destinationTitle={destinationTitle}/></div>
       <div css={rightsidebar}>여긴 추천장소가 들어갈 자리</div>
